58a889ab2726fb8471cd2a549e603f664f22ab8b
Sahana bk <sahanabukinakere.ai23@bmsce.ac.in>
2025-07-31 11:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+@app.route('/')
+def home():
+    return "FinGuard backend is running!"
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file found'})
+    file = request.files['file']
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(filepath)
+    return jsonify({'message': 'File uploaded successfully', 'filepath': filepath})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
